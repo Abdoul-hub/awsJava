071ec536bc732615b2e4cb85f8d1bca2f5d236fa
Abdoul Karim TOGOLA <AbdoulKarim.TOGOLA@orangemali.com>
2022-03-29 12:58:21 +0000

    update

---
+++ src/test/java/com/cabral/awstest/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabral.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
